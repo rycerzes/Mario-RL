0fcaee6ff024d62e840781e860255d8d35dbf1f9
swappy <59965507+rycerzes@users.noreply.github.com>
2024-02-14 21:46:30 +0530

    misc changes
    fixed versioning

--- rand_action.py
+++ rand_action.py
@@ -2,11 +2,7 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY
 from nes_py.wrappers import JoypadSpace
 
-from wrappers import apply_wrappers
-from agent import Agent
-
-ENV_NAME = 'SuperMarioBros-1-1-v0'
-SHOULD_TRAIN = True
+ENV_NAME = 'SuperMarioBros-1-1-v3'
 DISPLAY = True
 NUM_OF_EPISODES = 50_000
 
@@ -14,20 +10,11 @@ NUM_OF_EPISODES = 50_000
 env = gym_super_mario_bros.make(ENV_NAME, render_mode='human' if DISPLAY else 'rgb_array', apply_api_compatibility=True)
 env = JoypadSpace(env, RIGHT_ONLY)
 
-env = apply_wrappers(env)
-
-agent = Agent(input_dims=env.observation_space.shape, num_actions=env.action_space.n)
-
 for i in range(NUM_OF_EPISODES):
     done = False
     state, _ = env.reset()
     while not done:
-        a = agent.choose_action(state)
+        a = env.action_space.sample()  # Take random action
         new_state, reward, done, truncated, info  = env.step(a)
-        
-        agent.store_in_memory(state, a, reward, new_state, done)
-        agent.learn()
-
         state = new_state
-
 env.close()
