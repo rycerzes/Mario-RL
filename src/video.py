8e069eac85f034666ef20822dce45d6537a21832
Sainath Dey <123927607+Dopamine-Max@users.noreply.github.com>
2024-03-05 23:15:27 +0530

    Create video.py

---
+++ src/video.py
@@ -0,0 +1,20 @@
+from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
+import gymnasium as gym
+
+
+def record_video (env_id, model, video_length=700, prefix="", video_folder="videos/"):
+    eval_env = DummyVecEnv([lambda: gym.make(env_id, render_mode="rgb_array")])
+    eval_env = VecVideoRecorder(
+        eval_env,
+        video_folder=video_folder,
+        record_video_trigger=lambda step: step == 0,
+        video_length=video_length,
+        name_prefix=prefix,
+    )
+
+    obs = eval_env.reset()
+    for _ in range(video_length):
+        action, _ = model.predict(obs, deterministic=False)
+        obs, _, _, _ = eval_env.step(action)
+
+    eval_env.close()
